cbaf20e4048faf6a422368f3b2c4e4f795745c70
Adam Ambrosius <aambrosius@bitbucket.org>
2016-11-26 15:44:58 -0600

    Initial Commit

--- GorrettiBoutique/Controllers/HomeController.cs
+++ GorrettiBoutique/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web.Mvc;
 using G.Models;
 
-namespace G.Controllers
+namespace GorrettiBoutique.Controllers
 {
     public class HomeController : Controller
     {
@@ -15,11 +15,13 @@ namespace G.Controllers
 
         public ActionResult Shop()
         {
-            List<ShopInfo> shopInfo = new List<ShopInfo> {
+            var shopInfo = new List<ShopInfo> {
                 new ShopInfo("~/Images/Shop/Dice.jpg", "Dice","Why use boring white dice? Add color to make any game "
                                 + "with dice more intersting.", "$15.00", 1),
                 new ShopInfo("~/Images/Shop/Mario.jpg", "Mario", "Mario is a collectors item. There were only 20 created "
                                 + "and we only have 10 left.", "$5.08", 10),
+                new ShopInfo("~/Images/Shop/dress.jpg","Dress", "This dress looks great for any occasion. "
+                                + "Check out this dress", "$29.99", 20),
                 new ShopInfo("~/Images/Shop/APenguin.jpg","Penguin", "Penguins are the perfect gift for any kid. "
                                 + "Check out the adorable penguin", "$1.99", 50)
             };
