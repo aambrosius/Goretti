852f3ff7f4eafc2bd3666740e87619586e1d5401
Adam Ambrosius <aambrosius@bitbucket.org>
2016-11-26 15:30:17 -0600

    Initial Commit

--- GorrettiBoutique/Controllers/HomeController.cs
+++ GorrettiBoutique/Controllers/HomeController.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using G.Models;
 
-namespace GorrettiBoutique.Controllers
+namespace G.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            List<ShopInfo> shopInfo = new List<ShopInfo> {
+                new ShopInfo("~/Images/Shop/Dice.jpg", "Dice","Why use boring white dice? Add color to make any game "
+                                + "with dice more intersting.", "$15.00", 1),
+                new ShopInfo("~/Images/Shop/Mario.jpg", "Mario", "Mario is a collectors item. There were only 20 created "
+                                + "and we only have 10 left.", "$5.08", 10),
+                new ShopInfo("~/Images/Shop/APenguin.jpg","Penguin", "Penguins are the perfect gift for any kid. "
+                                + "Check out the adorable penguin", "$1.99", 50)
+            };
+
+            //List<ShopInfo> shopInfo = new ShopInfo();
+            //shopInfo.Add
+
+            ViewBag.ShopInfo = shopInfo;
+            ViewBag.Images = Directory.EnumerateFiles(Server.MapPath("~/Images/Shop")).Select(fn => "~/Images/Shop/" + Path.GetFileName(fn));
             return View();
         }
 
---
+++ GorrettiBoutique/Models/ShopInfo.cs
@@ -0,0 +1,37 @@
+﻿namespace G.Models
+{
+    public class ShopInfo
+    {
+        //public string Image { get; set; }
+        //public string Title { get; set; }
+        //public string Description { get; set; }
+        //public string Price { get; set; }
+        //public int Quantity { get; set; }
+        public ShopInfo(string image, string title, string description, string price, int quantity)
+        {
+            Image = image;
+            Title = title;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+        #region Variables
+
+        private string _image;
+        private string _title;
+        private string _description;
+        private string _price;
+        private int _quantity;
+
+        #endregion
+
+        #region Getters/Settters
+
+        public string Image { get { return _image; } set { _image = value; } }
+        public string Title { get { return _title; } set { _title = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+        public string Price { get { return _price; } set { _price = value; } }
+        public int Quantity { get { return _quantity; } set { _quantity = value; } }
+        #endregion
+    }
+}
